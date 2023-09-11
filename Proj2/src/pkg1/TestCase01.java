a1dfdbc809db212a63066d755a69cc95a5153f88
manith mathews <mathewsmanith@gmail.com>
2023-09-11 20:46:37 +1000

    1st commit to Sep2023

---
+++ Proj2/src/pkg1/TestCase01.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class TestCase01 {
+	
+	int x=300;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TestCase01 obj1=new TestCase01();
+		System.out.println(obj1.x); 
+	}
+
+}
